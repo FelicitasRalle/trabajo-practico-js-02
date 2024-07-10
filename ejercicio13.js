b9e507d21775a15101fc25efe8736811ecc8f677
unknown <felicitasralle2009@gmail.com>
2024-07-10 00:07:16 -0300

    Se agrego el ejercicio 13

---
+++ ejercicio13.js
@@ -0,0 +1,6 @@
+let texto = prompt("Ingrese un texto:");
+
+let textoMayusculas = texto.toUpperCase();
+
+console.log("Texto en mayúsculas: " + textoMayusculas);
+alert("Texto en mayúsculas: " + textoMayusculas);
